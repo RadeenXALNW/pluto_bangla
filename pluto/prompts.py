588440aba9ce6a6342ba578916e4ca957df4166c
Radeen Mostafa <66905164+RadeenXALNW@users.noreply.github.com>
2024-07-27 17:57:01 +0600

    Update prompts.py

--- pluto/prompts.py
+++ pluto/prompts.py
@@ -28,44 +28,40 @@ Now write a training sample and return it as a json, as seen above."""
 
 
 TREE_GENERATION_PROMPT = """I want to train a large language model and I am using another, bigger large language model to generate training data for this. However, if we always ask the bigger model to generate training data with the same prompt, it will end up generating very repetitive training samples. Therefore, we will slightly modify our prompt for each sampling procedure according to some aspects. For instance, when asking the model to generate news articles, we could modify the prompt to let the model tell news articles about particular topics, such as business or politics. To further generate training data, we will do this recursively, and generate submodifications to the prompt. For instance, within the domain of business, we could adapt the prompt to generate news about the stock market or business scandals, and within politics, we could ask the model to generate articles for subtopics like elections or climate policy. We do this recursively, and therefore, we get a tree-like structure of topics.
-Your job is the following: I will give you a path of nodes down the topic tree - you should then come up with a list of new subtopics for this given node and return it as a python list. Here are a few examples of what your outputs should look like, related to the news example I just gave you:
+Your job is the following: I will give you a path of nodes down the topic tree - you should then come up with a list of new subtopics for this given node and return it as a python list. Remember, all has to be in Bangla. And it has to be grammatical and spelling error free. Here are a few examples of what your outputs should look like, related to the news example I just gave you:
 
 Example 1:
-node path: "News Topics" -> "Sports" -> "Football"
+node path: "সংবাদ বিষয়বস্তু" -> "স্বাস্থ্য" -> "খাদ্য"
 desired number of subtopics: 5
-subtopics: ["college football", "football stadiums", "health consequences football", "Seattle Seahawks", "football sponsorships"]
-
+subtopics: ["পুষ্টিকর খাবার", "স্বাস্থ্যকর রেসিপি", "খাবারের উপাদান", "ডায়েট প্ল্যান", "স্বাস্থ্যকর খাদ্যাভ্যাস"]
 
 Example 2:
-node path: "News Topics" -> "Entertainment" -> "Movies" -> "Star Portraits"
+node path: "সংবাদ বিষয়বস্তু" -> "প্রযুক্তি" -> "গ্যাজেট"
 desired number of subtopics: 8
-subtopics: ["Tom Hanks", "Meryl Streep", "Leonardo DiCaprio", "Jennifer Lawrence", "Denzel Washington", "Charlize Theron", "Robert Downey Jr.", "Emma Stone"]
-
+subtopics: ["স্মার্টফোন", "ল্যাপটপ", "ট্যাবলেট", "পরিধানযোগ্য প্রযুক্তি", "ড্রোন", "স্মার্টওয়াচ", "ব্লুটুথ স্পিকার", "ভার্চুয়াল রিয়ালিটি হেডসেট"]
 
 Here are three new examples, this time for generating smalltalk topics for a friendly chat assistant:
 
 Example 1:
-node path: "Small Talk Topics"
+node path: "সামান্য আলাপের বিষয়বস্তু"
 desired number of subtopics: 7
-subtopics: ["weather", "weekend plans", "hobbies", "family", "books", "food", "music"]
+subtopics: ["আবহাওয়া", "সপ্তাহান্তের পরিকল্পনা", "শখ", "পরিবার", "বই", "খাবার", "সঙ্গীত"]
 
 Example 2:
-node path: "Small Talk Topics" -> "Family"
+node path: "সামান্য আলাপের বিষয়বস্তু" -> "পরিবার"
 desired number of subtopics: 5
-subtopics: ["parents", "grandparents", "siblings", "family traditions", "family vacations"]
+subtopics: ["মা-বাবা", "দাদা-দাদি", "ভাই-বোন", "পারিবারিক ঐতিহ্য", "পারিবারিক ছুটি"]
 
 Example 3:
-node path: "Small Talk Topics" -> "Hobbies" -> "Cooking"
+node path: "সামান্য আলাপের বিষয়বস্তু" -> "শখ" -> "রান্না"
 desired number of subtopics: 6
-subtopics: ["recipes", "asian food", "favourite dishes", "cookbooks", "kitchen gadgets", "vegan cooking"]
-
+subtopics: ["রেসিপি", "এশিয়ান খাবার", "প্রিয় খাবার", "রান্নার বই", "রান্নাঘরের গ্যাজেট", "ভেগান রান্না"]
 
 Here is a description / the system prompt for the model we want to train:
 
 <system_prompt>
 {{{{system_prompt}}}}
 </system_prompt>
-
 
 Here is your topic input. When generating subtopics, remain somewhat vague. Things can only be tangentially related and they don't have to be interpreted in a single way. Importantly, make sure that the subtopics fit the system prompt, if one was supplied:
 node path: {{{{subtopics_list}}}}
